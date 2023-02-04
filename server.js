b23907c143496bc6cb3780339713e74571404744
UnderTaker2k6 <86512807+AshAritra@users.noreply.github.com>
2023-02-04 16:16:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-    console.dir(req.url);
-
-    // will get you  '/' or 'index.html' or 'css/styles.css' ...
-    // • you need to isolate extension
-    // • have a small mimetype lookup array/object
-    // • only there and then reading the file
-    // •  delivering it after setting the right content type
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    res.end('ok');
-}).listen(3001);
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false})); app.use(express.static(path.join(__dirname, 'public')));
+app.use('/test',route);
+app.use((req, res,next)=>{
+   res.status(404).send('<h1> Page not found </h1>');
+});
+const server = http.createServer(app);
+server.listen(3000);
