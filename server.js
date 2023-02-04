678a263c4661bb371cc7dc5a67004b9e6c108f3f
UnderTaker2k6 <86512807+AshAritra@users.noreply.github.com>
2023-02-04 15:42:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fs.readFile('./index.html', function (err, html) {
     if (err) throw err;    
 
     http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html", "text/css", "text/javascript", "image/png"});  
+        response.writeHeader(200, {"Content-Type": "application/json"});  
         response.write(html);  
         response.end();  
     }).listen(PORT);
