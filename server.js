1405c6c6d924f2ed72ebccc10d0870ab07f23dc3
UnderTaker2k6 <86512807+AshAritra@users.noreply.github.com>
2023-02-04 15:59:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use('/html',(req,res,next)=>{
-res.sendFile(path.join(__dirname,'index.html');
+res.sendFile(path.join(__dirname,'index.html'));
 });
 app.listen(3000);
