23f1f2640f3849235e8ace9a18eac1f18f7a69b7
UnderTaker2k6 <86512807+AshAritra@users.noreply.github.com>
2023-02-04 11:32:50 +0000

    deleted:    package-lock.json 	deleted:    package.json 	deleted:    routes.js 	deleted:    server.js

--- routes.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-router.get('/add-username', (req, res,next)=>{
-   // res.send('<form action="/test/post-username" method="POST"> <input type="text" name="username">    <button type="submit"> Send </button> </form>');
-   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
